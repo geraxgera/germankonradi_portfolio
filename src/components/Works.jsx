2282bf70956e70142fec4f435390e53b3ab44634
geraxgera <gera.konradi@gmail.com>
2023-01-11 20:40:47 +0100

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,7 +6,7 @@ import works from "../data/works";
 function Works() {
   return (
     <div className="py-12">
-      <SectionTitle id="works">Recent Works</SectionTitle>
+      <SectionTitle id="works">Aktuelle Arbeiten</SectionTitle>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       {works.map(work => (
         <WorkItem
